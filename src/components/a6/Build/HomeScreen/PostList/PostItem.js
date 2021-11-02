ac14934720ef316f5c34fea9b0d8325bdde2e309
LumingYang <yang.lum@northeastern.edu>
2021-11-02 17:52:33 -0500

    resolve netlify error

--- src/components/a6/Build/HomeScreen/PostList/PostItem.js
+++ src/components/a6/Build/HomeScreen/PostList/PostItem.js
@@ -42,27 +42,3 @@ const PostItem = ({post}) => {
     );
 }
 export default PostItem;
-
-
-
-
-// <div className="card text-white bg-light mb-1" style="max-width: 20rem;">
-//     <div className="card-header">Header</div>
-//     <div className="card-body">
-//         <h4 className="card-title">Dark card title</h4>
-//         <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-//             content.</p>
-//     </div>
-// </div>
-
-// <div className="wd-post">
-//     <img src={post.avatar} className="wd-avatar"/>
-//     <div className="wd-white-lg-text">{post.userName}<Span className="wd-gray-text">@{post.handle} {post.time}</Span></div>
-//     <div className="wd-white-sm-text">{post.summary}</div>
-//     <img className="wd-post-img" src={post.image}/>
-//     <div className="wd-post-bottom wd-white-post-text">${post.content}</span></div>
-// <a href="#"><i className="far fa-comment wd-icon-1"></i><span className="wd-gray-text wd-icon-num1">{post.commentNo}</span></a>
-// <a href="#"><i className="fas fa-retweet wd-icon-2"></i><span className="wd-gray-text wd-icon-num2">{post.retweetNo}</span></a>
-// <a href="#"><i className="far fa-heart wd-icon-3"></i><span className="wd-gray-text wd-icon-num3">{post.likeNo}</span></a>
-// <a href="#"><i className="fas fa-external-link-alt wd-icon-4"></i></a>
-// </div>
--- src/components/a6/Build/index.js
+++ src/components/a6/Build/index.js
@@ -1,8 +1,5 @@
 
 import React from "react";
-import NavigationSidebar from "./NavigationSidebar";
-import WhoToFollowList from "./WhoToFollowList";
-import PostSummaryList from "./PostSummaryList";
 import ExploreScreen from "./ExploreScreen/ExploreScreen";
 import HomeScreen from "./HomeScreen/HomeScreen";
 const Build = () => {
@@ -14,8 +11,4 @@ const Build = () => {
     )
 };
 export default Build;
-
-// <NavigationSidebar active="home"/>
-// <WhoToFollowList/>
-// <PostSummaryList/>
 
